6637e427d0ba0fde180de5fe4cf0dd371a45f288
Richard Knight <rjhknight@gmail.com>
2021-08-31 20:25:48 +1000

    Removing tmp

--- name_tmp.R
+++
@@ -1,4 +0,0 @@
-mb03vd<- function(n,p,ilo,a,tau,info)
-{n <- as.integer(n)
-ilo <- as.integer(ilo)p <- dim(a)[3]tau <- array(as.integer(0), c(max(1,n-1),p))
-info <- as.integer(0)res <- .Fortran(MB03VD,N=n,P=p,ILO=ilo,A=a,TAU=tau,INFO=info)return (list(a = res$A,tau = res$TAU,info = res$INFO))
